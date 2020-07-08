3348d6f73e268385afc4859c16969c6a6d59426e
JoongSeokD <ljseokd@gmail.com>
2020-07-08 16:06:19 +0900

    AccountController

--- src/main/java/me/ljseokd/studyspringbootwebapp/account/AccountController.java
+++ src/main/java/me/ljseokd/studyspringbootwebapp/account/AccountController.java
@@ -4,12 +4,16 @@ import me.ljseokd.studyspringbootwebapp.dto.SignUpForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(("/sign-up"))
 public class AccountController {
 
-    @GetMapping("/sign-up")
-    public String signUp(@ModelAttribute SignUpForm signUpForm){
+    @GetMapping
+    public String signUp(@ModelAttribute("signUpForm") SignUpForm signUpForm){
         return "account/sign-up";
     }
+
 }

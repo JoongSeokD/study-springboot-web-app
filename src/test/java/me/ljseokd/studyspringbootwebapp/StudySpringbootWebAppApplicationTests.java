ce3178afdd467c781726867be9e644d170235944
JoongSeokD <ljseokd@gmail.com>
2020-07-08 15:09:02 +0900

    base file

---
+++ src/test/java/me/ljseokd/studyspringbootwebapp/StudySpringbootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ljseokd.studyspringbootwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

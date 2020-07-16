48df33b20683a4b0b3c95bc420737b25112822f8
JoongSeokD <ljseokd@gmail.com>
2020-07-16 15:52:15 +0900

    스터디 설정 - 소개 수정

--- src/main/java/me/ljseokd/studyspringbootwebapp/mail/HtmlEmailService.java
+++ src/main/java/me/ljseokd/studyspringbootwebapp/mail/HtmlEmailService.java
@@ -33,6 +33,7 @@ public class HtmlEmailService implements EmailService{
             log.info("sent email : {}", emailMessage.getMessage());
         } catch (MessagingException e) {
             log.error("failed to send email", e);
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/me/ljseokd/studyspringbootwebapp/study/StudyService.java
+++ src/main/java/me/ljseokd/studyspringbootwebapp/study/StudyService.java
@@ -3,8 +3,12 @@ package me.ljseokd.studyspringbootwebapp.study;
 import lombok.RequiredArgsConstructor;
 import me.ljseokd.studyspringbootwebapp.domain.Account;
 import me.ljseokd.studyspringbootwebapp.domain.Study;
+import me.ljseokd.studyspringbootwebapp.study.form.StudyDescriptionForm;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +16,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class StudyService {
 
     private final StudyRepository studyRepository;
+    private final ModelMapper modelMapper;
 
     public Study createNewStudy(Study study, Account account) {
         Study newStudy = studyRepository.save(study);
         newStudy.addManager(account);
         return newStudy;
 
+    }
+
+    public Study getStudyToUpdate(Account account, String path) {
+        Study study = getStudy(path);
+        if (!account.isManagerOf(study)){
+            throw new AccessDeniedException("해당 기능을 사용할 수 없습니다.");
+        }
+        return study;
+    }
+
+    public Study getStudy(String path) {
+        Study study = studyRepository.findByPath(path);
+        if (study == null){
+            throw new IllegalArgumentException(path + "에 해당하는 스터디가 없습니다.");
+        }
+        return study;
+    }
+
+    public void updateStudyDescription(Study study, StudyDescriptionForm studyDescriptionForm){
+        modelMapper.map(studyDescriptionForm, study);
     }
 }
